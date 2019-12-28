730f52aa87cf786e8c5a9f0a2633b66b124e6885
Viktor Pyskunov <vpyskunov@gmail.com>
2019-12-28 15:13:35 +0200

    refactor to class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,22 @@
 import React, { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { AppState } from '../types'
-import { fetchById } from '../services/fetchById'
+import { Fetcher } from '../services/fetchById'
 import '../styles/index.css'
 
 const App = () => {
 	const users = useSelector((state: AppState) => state.users)
 	const fetchUser = useCallback(() => {
-		fetchById(1)
+		const fetcher = new Fetcher()
+		fetcher.fetchById(1)
 			.then(user => console.log('user', user))
 	}, [])
 	
 	return (
 		<div>
-			{Object.keys(users).map(key => <div key={key}>{users[key].fullName}</div>)}
-			<h1>Hello World!</h1>
+			<h1>Users</h1>
 			<button onClick={fetchUser}>Fetch user</button>
+			{Object.keys(users).map(key => <div key={key}>{users[key].fullName}</div>)}
 		</div>
 		)
 	}
--- src/services/fetchById.ts
+++ src/services/fetchById.ts
@@ -5,27 +5,29 @@ import { User } from 'types'
 const USER_ENDPOINT = 'http://localhost:8222/users/'
 
 interface IFetcher<T> {
-  (id: number): Promise<T | undefined>
+  fetchById(id: number): Promise<T | undefined>
 }
 
-export const fetchById: IFetcher<User> = (id: number): Promise<User | undefined> => {
-  if (id <= 0) {
-    return Promise.reject(new Error(`id should be greater than 0`))
-  }
-  
-  return fetch(`${USER_ENDPOINT}${id}`)
-  .then(response => {
-    if (response.status < 200 || response.status >= 300) {
-      return Promise.reject(new Error(response.statusText))
+export class Fetcher implements IFetcher<User> {
+  fetchById = (id: number): Promise<User | undefined> => {
+    if (id <= 0) {
+      return Promise.reject(new Error(`id should be greater than 0`))
     }
     
-    return response.json()
-  })
-  .then(user => {
-    // Save to Redux
-    const store = getStore()
-    store.dispatch(saveUser(user))
-
-    return user
-  })
+    return fetch(`${USER_ENDPOINT}${id}`)
+    .then(response => {
+      if (response.status < 200 || response.status >= 300) {
+        return Promise.reject(new Error(response.statusText))
+      }
+      
+      return response.json()
+    })
+    .then(user => {
+      // Save to Redux
+      const store = getStore()
+      store.dispatch(saveUser(user))
+  
+      return user
+    })
+  }
 }
